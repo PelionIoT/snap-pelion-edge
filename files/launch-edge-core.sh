c32a5bf9b26e28ee59c000d06f1df2ac29591d0e
Nic Costa <nic.costa@arm.com>
2019-07-01 11:14:41 -0500

    add convenience app for resetting storage
    For convenience, add a new app "pelion-edge.edge-core-reset-storage"
    to call edge-core with the --reset-storage parameter.
    
    While the ability to modify edge-core startup parameters is useful,
    it's inconvenient.
    
    In the case of resetting storage, we only want to run edge-core
    this way one time and we don't want the parameters to stay in place
    after a reboot.

--- files/launch-edge-core.sh
+++ files/launch-edge-core.sh
@@ -3,9 +3,17 @@
 # SNAP_DATA: /var/snap/pelion-edge/current/
 CONF_FILE=${SNAP_DATA}/edge-core.conf
 
+# defaults
 ARGS=""
+
+# read from CLI args given by snapcraft.yaml->apps->command
+if [ "$#" -gt "0" ]; then
+    ARGS="${ARGS} $@"
+fi
+
+# read from conf file
 if [ -f "${CONF_FILE}" ]; then
-    ARGS=$(cat "${CONF_FILE}")
+    ARGS="${ARGS} $(cat "${CONF_FILE}")"
 fi
 
 exec ${SNAP}/edge-core ${ARGS}
