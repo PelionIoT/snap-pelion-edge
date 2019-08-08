18a9567bb1cf77bf630add06adf8087c9b3522ab
Jordan Rife <jordan.rife@arm.com>
2019-08-08 14:57:59 -0500

    Add kubelet and CNI components to pelion snap

---
+++ files-dumped/launch-kubelet.sh
@@ -0,0 +1,19 @@
+
+DEVICE_ID=`jq -r .deviceID ${SNAP_DATA}/var/lib/edge_gw_identity/identity.json`
+
+if [ $? -ne 0 ]; then
+    echo "Unable to extract device ID from identity.json"
+
+    exit 1
+fi
+
+exec ${SNAP}/wigwag/system/bin/kubelet \
+    --root-dir=${SNAP_DATA}/var/lib/kubelet \
+    --fail-swap-on=false \
+    --image-pull-progress-deadline=2m \
+    --hostname-override=${DEVICE_ID} \
+    --kubeconfig=${SNAP}/wigwag/system/var/lib/kubelet/kubeconfig \
+    --cni-bin-dir=${SNAP}/wigwag/system/opt/cni/bin \
+    --cni-conf-dir=${SNAP}/wigwag/system/etc/cni/net.d \
+    --network-plugin=cni \
+    --register-node=true
