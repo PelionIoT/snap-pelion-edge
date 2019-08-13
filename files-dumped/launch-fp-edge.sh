a192af8b6de27c9435fbcf65b91d9617c17c2493
Jordan Rife <jordan.rife@arm.com>
2019-08-13 13:55:36 -0500

    save

--- files-dumped/launch-fp-edge.sh
+++ files-dumped/launch-fp-edge.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # SNAP_DATA: /var/snap/pelion-edge/current/
-CONF_FILE=${SNAP_DATA}/fp-edge.conf
+CONF_FILE=${SNAP}/fp-edge.conf
 
 # defaults
 ARGS=""
@@ -17,7 +17,7 @@ if [ -f "${CONF_FILE}" ]; then
 fi
 
 mkdir -p /userdata/edge_gw_config
-jq -r .ssl.ca.ca /userdata/edge_gw_config/identity.json > /userdata/edge_gw_config/ca.pem
-jq -r .ssl.server.certificate /userdata/edge_gw_config/identity.json > /userdata/edge_gw_config/kubelet.pem
-jq -r .ssl.server.key userdata/edge_gw_config/identity.json > /userdata/edge_gw_config/kubelet-key.pem
+jq -r .ssl.ca.ca ${SNAP}/wigwag/userdata/edge_gw_config/identity.json > /userdata/edge_gw_config/ca.pem
+jq -r .ssl.server.certificate ${SNAP}/wigwag/userdata/edge_gw_config/identity.json > /userdata/edge_gw_config/kubelet.pem
+jq -r .ssl.server.key ${SNAP}/wigwag/userdata/edge_gw_config/identity.json > /userdata/edge_gw_config/kubelet-key.pem
 exec ${SNAP}/wigwag/system/bin/fp-edge ${ARGS}
---
+++ files-dumped/launch-kubelet.sh
@@ -0,0 +1,3 @@
+
+DEVICE_ID=`jq -r .deviceID ${SNAP}/wigwag/userdata/edge_gw_config/identity.json`
+${SNAP}/wigwag/system/bin/kubelet --image-pull-progress-deadline=2m --hostname-override=${DEVICE_ID} --kubeconfig=${SNAP}/wigwag/system/var/lib/kubelet/kubeconfig --cni-bin-dir=${SNAP}/wigwag/system/opt/cni/bin --cni-conf-dir=${SNAP}/wigwag/system/etc/cni/net.d --network-plugin=cni --register-node=true --v=10
