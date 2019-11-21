9653a4ed12371fe50752534ac13169d79ee94c61
Jordan Rife <jordan.rife@arm.com>
2019-11-21 13:14:17 -0600

    Use gateways and edge-k8s domain in the pelion cloud instead of hard-coded sandbox address

--- files-dumped/launch-fp-edge.sh
+++ files-dumped/launch-fp-edge.sh
@@ -2,10 +2,11 @@
 mkdir -p ${SNAP_DATA}/userdata/edge_gw_config
 jq -r .ssl.server.certificate ${SNAP_DATA}/var/lib/edge_gw_identity/identity.json > ${SNAP_DATA}/userdata/edge_gw_config/kubelet.pem
 jq -r .ssl.server.key ${SNAP_DATA}/var/lib/edge_gw_identity/identity.json > ${SNAP_DATA}/userdata/edge_gw_config/kubelet-key.pem
+EDGE_K8S_ADDRESS=`jq -r .edgek8sServicesAddress ${SNAP_DATA}/var/lib/edge_gw_identity/identity.json`
+GATEWAYS_ADDRESS=`jq -r .gatewayServicesAddress ${SNAP_DATA}/var/lib/edge_gw_identity/identity.json`
 
 exec ${SNAP}/wigwag/system/bin/fp-edge \
-    -proxy-uri=https://kaas-edge-nodes.arm.com \
-    -ca=${SNAP}/ca.pem \
+    -proxy-uri=${EDGE_K8S_ADDRESS} \
     -cert-strategy-options=cert=${SNAP_DATA}/userdata/edge_gw_config/kubelet.pem \
     -cert-strategy-options=key=${SNAP_DATA}/userdata/edge_gw_config/kubelet-key.pem \
-    -tunnel-uri=ws://127.0.0.1:8080/connect
+    -tunnel-uri=wss://${GATEWAYS_ADDRESS#"https://"}/edge-proxy/connect
