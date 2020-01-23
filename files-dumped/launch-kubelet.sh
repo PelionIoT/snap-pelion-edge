c1a29796a6663468f35a929be787fd47d68ed8a7
Nic Costa <nic.costa@arm.com>
2020-01-23 09:43:10 -0600

    remove edgenet as a service
    the kubelet service requires the edgenet docker network to be defined,
    but the mechanism used to set up edgenet can be simplified by having
    the kubelet startup script be responsible for creating the network,
    rather than treating "edgenet" as its own service.
    
    this clears up some confusion about edgenet showing up as an inactive
    service when viewing pelion-edge service statuses, since really it's
    just a config requirement.

--- files-dumped/launch-kubelet.sh
+++ files-dumped/launch-kubelet.sh
@@ -1,10 +1,14 @@
 
 DEVICE_ID=`jq -r .deviceID ${SNAP_DATA}/var/lib/edge_gw_identity/identity.json`
-
 if [ $? -ne 0 ]; then
     echo "Unable to extract device ID from identity.json"
-
     exit 1
+fi
+
+${SNAP}/launch-edgenet.sh
+if [ $? -ne 0 ]; then
+    echo "Unable to create edgenet docker network"
+    exit 2
 fi
 
 exec ${SNAP}/wigwag/system/bin/kubelet \
