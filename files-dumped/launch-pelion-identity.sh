23f922959261852e4dd4498bebe25152a7a71a90
Yash Goyal <yash.goyal@arm.com>
2020-02-21 12:55:54 -0600

    pelion-edge snap's userdata should reside in $SNAP_DATA/userdata
    Default SNAP_DATA is `/var/snap/pelion-edge/current`. Pelion Edge's configuration
    resides in `/userdata` on all other platfroms. To maintain the same file structure all
    userdata should be moved to `SNAP_DATA/userdata`. Thus,
    edge-core's mcc_config = `/var/snap/pelion-edge/current/userdata/mbed/mcc_config`
    maestro database = `/var/snap/pelion-edge/current/userdata/etc/maestroConfig.db`
    identity.json = `/var/snap/pelion-edge/current/userdata/edge_gw_config/identity.json`

--- files-dumped/launch-pelion-identity.sh
+++ files-dumped/launch-pelion-identity.sh
@@ -12,6 +12,6 @@ while [ $? -ne 0 ]
 do
   sleep 5
   sh $1/wwrelay-utils/debug_scripts/create-new-eeprom-with-self-signed-certs.sh\
-    $1 8081 $2/edge_gw_identity $2/pelion-edge/mcc_config
+    $1 8081 $2/edge_gw_config $2/mbed/mcc_config
 done
 
