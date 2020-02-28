313c4e8cdf7ceb3d0a21c0cddf052beb6d435aaa
OPpuolitaival <olli-pekka.puolitaival@arm.com>
2020-02-28 14:57:20 +0200

    Trigger right function from library side

--- EdgeJenkinsfile.groovy
+++ EdgeJenkinsfile.groovy
@@ -1,9 +1,5 @@
 #!/usr/bin/env groovy
 @Library('edge-ci') _
 
-/**
-Jenkinsfile for https://edge-jenkins.isgtesting.com/
-*/
-
-// This function is defined in https://github.com/ArmPelionEdge/edge-ci repository
-snapBuilder('syte-dev-credentials-os2')
+// Function is implemented in https://github.com/armPelionEdge/edge-ci/blob/master/vars/snapPelionEdge.groovy
+snapPelionEdge()
