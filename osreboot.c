6151a552e07401fca98d0211a5a0e0370cb45737
J. Michael Welsh <mike.welsh@arm.com>
2020-03-24 12:52:34 -0500

    Reboot function
    Adding an a custom reboot function to restart edge-core.

---
+++ osreboot.c
@@ -0,0 +1,11 @@
+#include "pal.h"
+#include <stdlib.h>
+#include <string.h>
+#include "pal_plat_rtos.h"
+#include "mbed.h"
+#include "entropy_poll.h"
+
+void pal_plat_osApplicationReboot(void)
+{
+    printf("hello\n");
+}
