c005ed4829b185408c12a4af4e3f9c34291c6733
J. Michael Welsh <mike.welsh@arm.com>
2020-03-26 17:37:08 -0500

    Real Reboot
    Adding the command to make /3/0/4 do a real reboot on the running device.

--- osreboot.c
+++ osreboot.c
@@ -1,9 +1,12 @@
 #include "mbed-trace/mbed_trace.h"
 #include <stdlib.h>
+#include <unistd.h>
+
 #define TRACE_GROUP "serv"
 
 void pal_plat_osApplicationReboot(void)
 {
     tr_warn("Restarting edge..\n");
-    exit(-666);
+    
+    system("dbus-send --system --print-reply --dest=org.freedesktop.login1 /org/freedesktop/login1 org.freedesktop.login1.Manager.Reboot boolean:true");
 }
