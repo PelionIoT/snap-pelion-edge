5da7640b27ea59a7b0bbb9d084b062e97bf3717e
Cristian Prundeanu <cristian.prundeanu@arm.com>
2020-04-01 16:00:19 -0500

    Split command line parsing into separate script
    Signed-off-by: Cristian Prundeanu <cristian.prundeanu@arm.com>

--- files/edge-core/scripts/arm_update_activate.sh
+++ files/edge-core/scripts/arm_update_activate.sh
@@ -6,57 +6,7 @@
 # The tarball signature and integrity info are handled by the update client
 # before this script is called.
 
-# SNAP_DATA: /var/snap/pelion-edge/current/
-# path copied from snap-pelion-edge/snap/hooks/install
-UPGRADE_DIR="${SNAP_DATA}/upgrades"
-UPGRADE_TGZ="${UPGRADE_DIR}/upgrade.tar.gz"
-UPGRADE_HDR="${UPGRADE_DIR}/header.bin"
-
-# The following variables may be provided in the environment, or overridden
-# by command line arguments. See example with full set of scripts in
-# mbed-edge/lib/mbed-cloud-client/update-client-hub/modules/pal-linux/scripts/generic/
-#
-# HEADER
-# FIRMWARE
-# LOCATION
-# OFFSET
-# SIZE
-
-while [ -n "$1" ]; do
-    shift_count=2
-    case "$1" in
-        -h|--header)
-            HEADER="$2"
-            ;;
-        -f|--firmware)
-            FIRMWARE="$2"
-            ;;
-        -l|--location)
-            LOCATION="$2"
-            ;;
-        -o|--offset)
-            OFFSET="$2"
-            ;;
-        -s|--size)
-            SIZE="$2"
-            ;;
-        -*)
-            echo >&2 "Unknown option: $1"
-            shift_count=1
-            ;;
-        *)
-            # the script can be called from pal_ext_imageActivationWorker()
-            # with a single argument: the firmware path
-            FIRMWARE="$1"
-            shift_count=1
-            ;;
-    esac
-    shift $shift_count
-done
-
-LOCATION=${LOCATION:-0}
-OFFSET=${OFFSET:-0}
-SIZE=${SIZE:-0}
+. arm_update_cmdline.sh
 
 mkdir -p "$UPGRADE_DIR"
 
---
+++ files/edge-core/scripts/arm_update_cmdline.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# This script is used for firmware upgrades. It parses the command line in
+# order to allow environment and command line parameter merging
+
+# SNAP_DATA: /var/snap/pelion-edge/current/
+# path copied from snap-pelion-edge/snap/hooks/install
+UPGRADE_DIR="${SNAP_DATA}/upgrades"
+UPGRADE_TGZ="${UPGRADE_DIR}/upgrade.tar.gz"
+UPGRADE_HDR="${UPGRADE_DIR}/header.bin"
+
+# The following variables may be provided in the environment, or overridden
+# by command line arguments. See example with full set of scripts in
+# mbed-edge/lib/mbed-cloud-client/update-client-hub/modules/pal-linux/scripts/generic/
+#
+# HEADER
+# FIRMWARE
+# LOCATION
+# OFFSET
+# SIZE
+
+while [ -n "$1" ]; do
+    shift_count=2
+    case "$1" in
+        -h|--header)
+            HEADER="$2"
+            ;;
+        -f|--firmware)
+            FIRMWARE="$2"
+            ;;
+        -l|--location)
+            LOCATION="$2"
+            ;;
+        -o|--offset)
+            OFFSET="$2"
+            ;;
+        -s|--size)
+            SIZE="$2"
+            ;;
+        -*)
+            echo >&2 "Unknown option: $1"
+            shift_count=1
+            ;;
+        *)
+            # the script can be called from pal_ext_imageActivationWorker()
+            # with a single argument: the firmware path
+            FIRMWARE="$1"
+            shift_count=1
+            ;;
+    esac
+    shift $shift_count
+done
+
+LOCATION=${LOCATION:-0}
+OFFSET=${OFFSET:-0}
+SIZE=${SIZE:-0}
