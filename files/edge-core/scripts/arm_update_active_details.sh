7516875e099557a8e23087762d600ce54c061b94
Cristian Prundeanu <cristian.prundeanu@arm.com>
2020-04-02 11:38:26 -0500

    Add arm_update_active_details script
    Signed-off-by: Cristian Prundeanu <cristian.prundeanu@arm.com>

---
+++ files/edge-core/scripts/arm_update_active_details.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script is used for firmware upgrades. It prepares the update
+# payload for use by the fake bootloader script.
+#
+# The tarball signature and integrity info are handled by the update client
+# before this script is called.
+
+. arm_update_cmdline.sh
+
+# copy header if present
+[ -n "$UPGRADE_HDR" ] && cp "$UPGRADE_HDR" "$HEADER"
