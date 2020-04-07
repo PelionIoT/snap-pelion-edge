e150e923e89c1a9f892fb6f8371cfe94043efcb9
Nic Costa <nic.costa@gmail.com>
2020-04-07 11:14:21 -0500

    update: fix syntax error in example runme.sh
    the keyword is "function" not "func".

--- update/runme.sh
+++ update/runme.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-func pre_update() {
+function pre_update() {
     echo "START pre-update"
     echo "END pre-update"
 }
 
-func update() {
+function update() {
     echo "START update"
     snap install --devmode pelion-edge_1.0_amd64.snap
     echo "END update"
 }
 
-func post_update() {
+function post_update() {
     echo "START post-update"
     cp platform_version ${SNAP_DATA}/etc/
     echo "END post-update"
 }
 
-func main() {
+function main() {
     pre_update "$@"
     update "$@"
     post_update "$@"
