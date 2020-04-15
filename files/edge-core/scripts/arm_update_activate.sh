9bb6199c5997510e488c8fce87870e29abf73b28
Cristian Prundeanu <cristian.prundeanu@arm.com>
2020-04-15 14:49:34 -0500

    Add fake installer.bin
    The C code (arm_uc_pal_linux_implementation_internal.c) is looking for
    a /tmp/installer.bin and throws an error if it isn't there. Its data
    doesn't seem to actually be used for anything, so we'll just generate
    a zero file for now.
    
    Signed-off-by: Cristian Prundeanu <cristian.prundeanu@arm.com>

--- files/edge-core/scripts/arm_update_activate.sh
+++ files/edge-core/scripts/arm_update_activate.sh
@@ -14,3 +14,7 @@ mkdir -p "$UPGRADE_DIR"
 [ -n "$HEADER" ] && cp "$HEADER" "$UPGRADE_HDR"
 # copy firmware to location to be applied by bootloader
 [ -n "$FIRMWARE" ] && cp "$FIRMWARE" "$UPGRADE_TGZ"
+# create fake installer.bin until we figure out if it's needed for anything
+# the size originates from struct _arm_uc_installer_details_t in arm_uc_types.h
+# see https://github.com/ARMmbed/mbed-edge/blob/master/lib/mbed-cloud-client/update-client-hub/modules/common/update-client-common/arm_uc_types.h#L74
+dd if=/dev/zero of=$UPGRADE_INS bs=68 count=1
