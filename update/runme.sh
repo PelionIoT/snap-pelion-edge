1df5248a9ef887f6eae15abbcd06bc806b0b4dae
Nic Costa <nic.costa@gmail.com>
2020-04-15 09:57:07 -0500

    simplify the example runme.sh
    the idea of having separate phases of the update process, such as
    pre-update, update, post-update, within the runme.sh wasn't fully
    designed in this first phase of FOTA.  therefore, having the example
    runme.sh break things out in this way is confusing.

--- update/runme.sh
+++ update/runme.sh
@@ -1,27 +1,6 @@
 #!/bin/bash
 set -eux
 
-
-function pre_update() {
-    echo "START pre-update"
-    echo "END pre-update"
-}
-
-function update() {
-    echo "START update"
-    snap install --devmode pelion-edge_1.0_amd64.snap
-    echo "END update"
-}
-
-function post_update() {
-    echo "START post-update"
-    echo "END post-update"
-}
-
-function main() {
-    pre_update "$@"
-    update "$@"
-    post_update "$@"
-}
-
-main "$@"
+echo "START runme.sh"
+snap install --devmode pelion-edge_1.0_amd64.snap
+echo "END runme.sh"
