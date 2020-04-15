4f1e7f2e49dbc66cb9edfe43f23323fe955cd885
Cristian Prundeanu <cristian.prundeanu@arm.com>
2020-04-15 16:47:54 -0500

    Add comment to runme.sh about upgrading pelion-edge
    Signed-off-by: Cristian Prundeanu <cristian.prundeanu@arm.com>

--- update/runme.sh
+++ update/runme.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#
+# This script will be run on FOTA execution
+#
+# You can update/install other snaps or update pelion-edge itself.
+#
+# If you update the pelion-edge snap, make sure it is the LAST step,
+# since that will stop the current pelion-edge snap (including this
+# script) and prevent it from running anything else (or returning to its
+# caller).
+#
+
 set -eux
 
 echo "START runme.sh"
