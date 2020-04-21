bc0c5e118667f07f5663bf1aaa2549652a11cb90
zhucebuliaolongchuan <cl4076@nyu.edu>
2020-04-21 12:59:17 -0500

    PELEDGE19-1428: add forwarding addresses feature for fog-proxy so other components like maestro and relay-term could send the requests through fog-proxy later

--- files/fog-proxy/scripts/launch-fp-edge.sh
+++ files/fog-proxy/scripts/launch-fp-edge.sh
@@ -2,12 +2,13 @@
 EDGE_K8S_ADDRESS=$(jq -r .edgek8sServicesAddress ${SNAP_DATA}/userdata/edge_gw_identity/identity.json)
 GATEWAYS_ADDRESS=$(jq -r .gatewayServicesAddress ${SNAP_DATA}/userdata/edge_gw_identity/identity.json)
 EDGE_PROXY_URI_RELATIVE_PATH=$(jq -r .edge_proxy_uri_relative_path ${SNAP_DATA}/fp-edge.conf.json)
-
 exec ${SNAP}/wigwag/system/bin/fp-edge \
     -proxy-uri=${EDGE_K8S_ADDRESS} \
-    -tunnel-uri=ws://${GATEWAYS_ADDRESS#"https://"}$EDGE_PROXY_URI_RELATIVE_PATH \
+    -tunnel-uri=ws://gateways.local$EDGE_PROXY_URI_RELATIVE_PATH \
     -cert-strategy=tpm \
     -cert-strategy-options=socket=/tmp/edge.sock \
     -cert-strategy-options=path=/1/pt \
     -cert-strategy-options=device-cert-name=mbed.LwM2MDeviceCert \
-    -cert-strategy-options=private-key-name=mbed.LwM2MDevicePrivateKey
+    -cert-strategy-options=private-key-name=mbed.LwM2MDevicePrivateKey \
+    -forwarding-addresses={\"gateways.local\":\"${GATEWAYS_ADDRESS#"https://"}\"}
+ls
