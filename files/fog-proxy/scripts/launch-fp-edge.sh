53fcc187f304ccde2bdc1c912eaeaec84b3f86fa
psppalash <palash.manishkumarshah@arm.com>
2020-04-24 16:11:32 -0500

    Update SHA

--- files/fog-proxy/scripts/launch-fp-edge.sh
+++ files/fog-proxy/scripts/launch-fp-edge.sh
@@ -2,9 +2,7 @@
 EDGE_K8S_ADDRESS=$(jq -r .edgek8sServicesAddress ${SNAP_DATA}/userdata/edge_gw_identity/identity.json)
 GATEWAYS_ADDRESS=$(jq -r .gatewayServicesAddress ${SNAP_DATA}/userdata/edge_gw_identity/identity.json)
 EDGE_PROXY_URI_RELATIVE_PATH=$(jq -r .edge_proxy_uri_relative_path ${SNAP_DATA}/fp-edge.conf.json)
-if grep -q "gateways.local" /etc/hosts; then
-    echo found entry
-else
+if ! grep -q "gateways.local" /etc/hosts; then
     echo "127.0.0.1 gateways.local" >> /etc/hosts
 fi
 exec ${SNAP}/wigwag/system/bin/fp-edge \
