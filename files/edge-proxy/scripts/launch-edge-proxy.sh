62070e1419a858d95b4c0c4331f96248f3bd2ef1
Nic Costa <nic.costa@gmail.com>
2020-08-04 15:19:28 -0500

    copy edge-proxy config during launch instead of install
    this allows edge-proxy to recover if SNAP_DATA is deleted.

--- files/edge-proxy/scripts/launch-edge-proxy.sh
+++ files/edge-proxy/scripts/launch-edge-proxy.sh
@@ -17,30 +17,36 @@
 # limitations under the License.
 # ----------------------------------------------------------------------------
 
+if [ ! -f "$SNAP_DATA/edge-proxy.conf.json" ]; then
+    cp "$SNAP/edge-proxy.conf.json" "$SNAP_DATA/edge-proxy.conf.json"
+fi
+
 EDGE_K8S_ADDRESS=$(jq -r .edgek8sServicesAddress ${SNAP_DATA}/userdata/edge_gw_identity/identity.json)
 GATEWAYS_ADDRESS=$(jq -r .gatewayServicesAddress ${SNAP_DATA}/userdata/edge_gw_identity/identity.json)
 DEVICE_ID=$(jq -r .deviceID ${SNAP_DATA}/userdata/edge_gw_identity/identity.json)
 EDGE_PROXY_URI_RELATIVE_PATH=$(jq -r .edge_proxy_uri_relative_path ${SNAP_DATA}/edge-proxy.conf.json)
 EXTERN_HTTP_PROXY=$(snapctl get edge-proxy.extern-http-proxy)
+
 if ! grep -q "gateways.local" /etc/hosts; then
     echo "127.0.0.1 gateways.local" >> /etc/hosts
 fi
+
 if ! grep -q "$DEVICE_ID" /etc/hosts; then
     echo "127.0.0.1 $DEVICE_ID" >> /etc/hosts
 fi
+
 if [[ $EXTERN_HTTP_PROXY = "" ]]; then
     EXTERN_ARG=
 else
     EXTERN_ARG=-extern-http-proxy-uri=$EXTERN_HTTP_PROXY
 fi
+
 if [[ $(snapctl get edge-proxy.debug) = "false" ]]; then
     echo "edge-proxy logging is disabled.  To see logs, run \"snap set pelion-edge edge-proxy.debug=true\" and restart edge-proxy"
     # this is known as bash exec redirection.
     # see https://www.tldp.org/LDP/abs/html/x17974.html
     exec >/dev/null 2>&1
 fi
-
-
 
 exec ${SNAP}/wigwag/system/bin/edge-proxy \
     -proxy-uri=${EDGE_K8S_ADDRESS} \
