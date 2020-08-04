4ba1154ae93258eea2e946d73965aa23a01453f1
Nic Costa <nic.costa@gmail.com>
2020-08-04 15:38:17 -0500

    copy maestro-config.yaml during startup instead of install
    this allows maestro to recover if SNAP_DATA is deleted

--- files/maestro/launch-maestro.sh
+++ files/maestro/launch-maestro.sh
@@ -12,4 +12,8 @@ if [ ! -d "$SNAP_DATA/userdata/etc" ]; then
     mkdir -p $SNAP_DATA/userdata/etc
 fi
 
+if [ ! -f "$SNAP_DATA/maestro-config.yaml" ]; then
+    cp "$SNAP/wigwag/wwrelay-utils/conf/maestro-conf/edge-config-dell5000-demo.yaml" "$SNAP_DATA/maestro-config.yaml"
+fi
+
 exec ${SNAP}/wigwag/system/bin/maestro -config $SNAP_DATA/maestro-config.yaml
