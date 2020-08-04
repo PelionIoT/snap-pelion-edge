99320eb8aa473917ced0eec2ebb39616e08a00c1
Nic Costa <nic.costa@gmail.com>
2020-08-04 15:21:23 -0500

    create wigwag/log in startup script instead of install
    this allows maestro to recover if SNAP_DATA is deleted

--- files/maestro/launch-maestro.sh
+++ files/maestro/launch-maestro.sh
@@ -4,4 +4,8 @@ if [ ! -f ${SNAP_DATA}/userdata/edge_gw_identity/identity.json ]; then
     exit 1
 fi
 
+if [ ! -d "$SNAP_DATA/wigwag/log" ]; then
+    mkdir -p $SNAP_DATA/wigwag/log
+fi
+
 exec ${SNAP}/wigwag/system/bin/maestro -config $SNAP_DATA/maestro-config.yaml
