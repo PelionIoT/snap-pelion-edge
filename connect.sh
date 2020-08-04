b25b6224af6127fecfb5e147679389ada2861325
Nic Costa <nic.costa@gmail.com>
2020-08-04 16:52:16 -0500

    explicitly rename system-files plug to system-files-logs
    it's best practice to give a descriptive plug name since we're
    specifying multiple attributes.

--- connect.sh
+++ connect.sh
@@ -26,7 +26,7 @@ sudo snap connect pelion-edge:support             :docker-support
 sudo snap connect pelion-edge:firewall-control    :firewall-control
 sudo snap connect pelion-edge:docker-cli          pelion-edge:docker-daemon
 sudo snap connect pelion-edge:log-observe         :log-observe
-sudo snap connect pelion-edge:system-files        :system-files
+sudo snap connect pelion-edge:system-files-logs   :system-files
 sudo snap connect pelion-edge:kernel-module-observe :kernel-module-observe
 sudo snap connect pelion-edge:system-trace        :system-trace
 sudo snap connect pelion-edge:system-observe      :system-observe
