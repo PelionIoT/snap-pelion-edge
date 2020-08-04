2baca94f92903b913c0b26c048973769d54f2163
Nic Costa <nic.costa@gmail.com>
2020-08-04 15:31:01 -0500

    create userdata/etc folder in start script
    this allows maestro to recover if SNAP_DATA is deleted.

--- files/maestro/launch-maestro.sh
+++ files/maestro/launch-maestro.sh
@@ -8,4 +8,8 @@ if [ ! -d "$SNAP_DATA/wigwag/log" ]; then
     mkdir -p $SNAP_DATA/wigwag/log
 fi
 
+if [ ! -d "$SNAP_DATA/userdata/etc" ]; then
+    mkdir -p $SNAP_DATA/userdata/etc
+fi
+
 exec ${SNAP}/wigwag/system/bin/maestro -config $SNAP_DATA/maestro-config.yaml
