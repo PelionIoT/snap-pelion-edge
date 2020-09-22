a3689323e9f2a5bbeab2096622a278bec99ca651
Kyle Stein <kyle.stein@arm.com>
2020-09-22 10:24:38 -0500

    Allow admin configuration of edgenet
    The docker network edgenet used by kubelet can now be configured
    by the snap environmnet variables kubelet.edgenet-subnet and
    kubelet.edgenet-gateway
    
    Signed-off-by: Kyle Stein <kyle.stein@arm.com>

--- files/kubelet/scripts/launch-edgenet.sh
+++ files/kubelet/scripts/launch-edgenet.sh
@@ -16,5 +16,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ----------------------------------------------------------------------------
+EDGENET_SUBNET=$(snapctl get kubelet.edgenet-subnet)
+EDGENET_GATEWAY=$(snapctl get kubelet.edgenet-gateway)
 
-docker network inspect edgenet &>/dev/null || docker network create --subnet=10.0.0.0/24 --gateway=10.0.0.1 edgenet
+docker network inspect edgenet &>/dev/null
+if [ $? -eq 0 ]; then
+	# edgenet already exists
+	CURRENT_SUBNET=$(docker network inspect --format='{{range .IPAM.Config}}{{.Subnet}}{{end}}' edgenet)
+	CURRENT_GATEWAY=$(docker network inspect --format='{{range .IPAM.Config}}{{.Gateway}}{{end}}' edgenet)
+	if [[ ${CURRENT_SUBNET} != ${EDGENET_SUBNET} || ${CURRENT_GATEWAY} != ${EDGENET_GATEWAY} ]]; then
+		docker network rm edgenet
+		docker network create --subnet=${EDGENET_SUBNET} --gateway=${EDGENET_GATEWAY} edgenet
+	fi
+else
+	# edgenet does not already exist
+	docker network create --subnet=${EDGENET_SUBNET} --gateway=${EDGENET_GATEWAY} edgenet
+fi
