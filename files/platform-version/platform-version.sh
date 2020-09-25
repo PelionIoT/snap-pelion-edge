771fbc05d56c3f973ca768988d811c54251b85d1
Kyle Stein <kyle.stein@arm.com>
2020-09-25 09:19:56 -0500

    Fix output format for platform-version
    Remove the trailing "-" (indicating stdin) from the output of
    md5sum
    
    Signed-off-by: Kyle Stein <kyle.stein@arm.com>

--- files/platform-version/platform-version.sh
+++ files/platform-version/platform-version.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -sS --unix-socket /run/snapd.socket http://localhost/v2/snaps -X GET | jq -r '[.result[]|{id,revision}]|sort_by(.id,.revision)' | md5sum
+curl -sS --unix-socket /run/snapd.socket http://localhost/v2/snaps -X GET | jq -r '[.result[]|{id,revision}]|sort_by(.id,.revision)' | md5sum | cut -f 1 -d " "
