68ceb772a004fe1c33dbc5b6a52dd4fc941e15ce
Kyle Stein <kyle.stein@arm.com>
2020-11-06 11:57:18 -0600

    Map version hash to human readable string
    Signed-off-by: Kyle Stein <kyle.stein@arm.com>

--- files/edge-core/scripts/launch-edge-core.sh
+++ files/edge-core/scripts/launch-edge-core.sh
@@ -18,6 +18,8 @@
 # ----------------------------------------------------------------------------
 
 PLATFORM_VERSION=${SNAP_DATA}/etc/platform_version
+READABLE_VERSION=${SNAP_DATA}/etc/readable_version
+VERSION_MAP=${SNAP_DATA}/etc/version_map.json
 
 # make sure the PAL_FS_MOUNT_POINT_PRIMARY directory exists so it can be populated
 # with mcc_config
@@ -33,9 +35,27 @@ fi
 # before we start edge-core, call the fake bootloader to apply any existing updates
 ${SNAP}/edge-core-bootloader.sh
 
+function map_version() {
+    HASH_VER=$1
+    READABLE=""
+    if [ -f $VERSION_MAP ]; then
+        READABLE=$(cat $VERSION_MAP | jq -r ".\"${HASH_VER}\"")
+        if [ "$READABLE" == "null" ]; then
+           READABLE=""
+        fi
+    fi
+    echo $READABLE
+}
+
 # Use the platform version script to generate a new MD5 hash
-# The platform version file is read by edge-core and populates LWM2M /10252/0/10
-${SNAP}/bin/platform-version.sh > $PLATFORM_VERSION
+# The platform version file is read by edge-core and populates LWM2M /10252/0/11
+PLAT_VER=$(${SNAP}/bin/platform-version.sh)
+echo $PLAT_VER > $PLATFORM_VERSION
+# Map platform version to human readable version string
+# The readable version file is read by edge-core and populates LWM2M /10252/0/10
+READ_VER=$(map_version $PLAT_VER)
+echo $READ_VER > $READABLE_VERSION
+
 
 CONF_FILE=${SNAP_DATA}/edge-core.conf
 if [ ! -f "${CONF_FILE}" ]; then
