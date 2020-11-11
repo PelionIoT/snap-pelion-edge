12194c1fb3b60d958db553487a81143735804710
Michael Ray <michael.ray@arm.com>
2020-11-11 12:40:49 -0600

    Rename armPelionEdge to PelionIoT

--- EdgeJenkinsfile.groovy
+++ EdgeJenkinsfile.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 @Library('edge-ci') _
 
-// Function is implemented in https://github.com/armPelionEdge/edge-ci/blob/master/vars/snapPelionEdge.groovy
+// Function is implemented in https://github.com/PelionIoT/edge-ci/blob/master/vars/snapPelionEdge.groovy
 snapPelionEdge()
