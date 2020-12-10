304d9b055a64765e2fcccc31ff4a7598b317137b
Olli-Pekka Puolitaival <olli-pekka.puolitaival@arm.com>
2020-12-10 15:30:05 +0200

    Test the fix

--- EdgeJenkinsfile.groovy
+++ EdgeJenkinsfile.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
-@Library('edge-ci') _
+@Library('edge-ci@snap_pr_check_fix') _
 
 // Function is implemented in https://github.com/armPelionEdge/edge-ci/blob/master/vars/snapPelionEdge.groovy
 snapPelionEdge()
