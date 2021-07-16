4c29282db6d4eea4d319911764eada56316d4648
Nic Costa <nic.costa@gmail.com>
2021-07-16 12:27:14 -0500

    edge-core: add provision-mode snap configuration
    Now that we have multiple versions of edge-core installed in the
    snap, we need to provide the user with a config option to choose
    which one to start.
    
    The new snap configuration variable is "edge-core.provision-mode"
    and valid values are "factory", "developer", "byoc", and "auto".
    The default is "auto".
    
    For factory mode, the expectation is that the mcc_config folder
    exists.
    
    For developer mode, the expectation is that the developer mode
    binary was built with developer credentials and installed into
    the snap.
    
    For byoc mode, the expectation is that the user has provided
    a CBOR file at /var/snap/pelion-edge/common/device.cbor.  See
    https://github.com/PelionIoT/mbed-edge/tree/master/edge-tool#readme
    for instructions on how to create a CBOR.
    
    The provision-mode is only useful if the device hasn't already
    been provisioned, i.e., if the mcc_config folder doesn't
    exist, because if the device is provisioned we just boot in
    factory mode regardless of the snap config option.
    
    To switch modes if the device is already provisioned:
        1. stop the pelion-edge snap services
        2. backup the existing mcc_config folder if desired
        3. delete the existing mcc_config folder
        4. set the snap config option edge-core.provision-mode
        5. provision credentials as in the expectations above
        6. start the pelion-edge snap services

--- files/edge-core/cmake/target.cmake
+++ files/edge-core/cmake/target.cmake
@@ -6,6 +6,8 @@ SET (PAL_TARGET_DEVICE x86_x64)
 MESSAGE ("Using SNAPCRAFT_PROJECT_NAME: $ENV{SNAPCRAFT_PROJECT_NAME}")
 add_definitions ("-DSNAPCRAFT_PROJECT_NAME=\"$ENV{SNAPCRAFT_PROJECT_NAME}\"")
 
+# the path to the mcc_config folder is needed by the launch-edge-core.sh script, so if this
+# path changes make sure to update the script.
 SET (PAL_FS_MOUNT_POINT_PRIMARY "\"/var/snap/$ENV{SNAPCRAFT_PROJECT_NAME}/current/userdata/mbed/mcc_config\"")
 SET (PAL_FS_MOUNT_POINT_SECONDARY "\"/var/snap/$ENV{SNAPCRAFT_PROJECT_NAME}/current/userdata/mbed/mcc_config\"")
 SET (PAL_UPDATE_FIRMWARE_DIR "\"/var/snap/$ENV{SNAPCRAFT_PROJECT_NAME}/current/upgrades\"")
