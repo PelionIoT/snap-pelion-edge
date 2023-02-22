7b3693ef60b85eca4b10ae72226e64a76a13117a
Janne Kiiskilä <janne.kiiskila@izumanetworks.com>
2023-02-22 15:13:33 +0200

    launch-fluent-bit.sh - shellcheck fixes
    Simple quotation fixes.
    
    /snap-pelion-edge/files/fluent-bit/scripts/launch-fluent-bit.sh:3:11: note: Double quote to prevent globbing and word splitting. [SC2086]
    /snap-pelion-edge/files/fluent-bit/scripts/launch-fluent-bit.sh:12:6: note: Double quote to prevent globbing and word splitting. [SC2086]
    /snap-pelion-edge/files/fluent-bit/scripts/launch-fluent-bit.sh:12:46: note: Double quote to prevent globbing and word splitting. [SC2086]

--- files/fluent-bit/scripts/launch-fluent-bit.sh
+++ files/fluent-bit/scripts/launch-fluent-bit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -f ${SNAP_DATA}/userdata/edge_gw_identity/identity.json ]; then
+if [ ! -f "${SNAP_DATA}/userdata/edge_gw_identity/identity.json" ]; then
     echo "identity.json does not exist"
     exit 1
 fi
@@ -9,4 +9,4 @@ if [ ! -f "$SNAP_DATA/fluent-bit.conf" ]; then
     cp "$SNAP/fluent-bit.conf" "$SNAP_DATA/fluent-bit.conf"
 fi
 
-exec ${SNAP}/wigwag/system/bin/fluent-bit -c $SNAP_DATA/fluent-bit.conf
+exec "${SNAP}/wigwag/system/bin/fluent-bit" -c "$SNAP_DATA/fluent-bit.conf"
