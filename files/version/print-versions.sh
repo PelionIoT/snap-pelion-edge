27ea8bb9bbf5e0383e0bc55a947d455aa9c34744
Janne Kiiskilä <janne.kiiskila@izumanetworks.com>
2023-02-22 15:33:56 +0200

    print-versions.sh - fix shellcheck findings
    Just a bunch of quotation, really.

--- files/version/print-versions.sh
+++ files/version/print-versions.sh
@@ -10,18 +10,18 @@ while [ -n "$1" ]; do
     shift
 done
 
-echo -n "relay-term: "; cat ${SNAP}/wigwag/etc/edge-node-modules.VERSION
-echo -n "pe-utils: "; ${SNAP}/wigwag/pe-utils/identity-tools/developer_identity/create-dev-identity.sh -V
-echo -n "edge-core: "; ${SNAP}/wigwag/mbed/edge-core --version
-echo -n "devicedb: "; ${SNAP}/wigwag/system/bin/devicedb -version
-echo -n "maestro: "; cat ${SNAP}/wigwag/etc/maestro.VERSION
-echo -n "maestro-shell: "; cat ${SNAP}/wigwag/etc/maestro-shell.VERSION
-echo -n "edge-proxy: "; cat ${SNAP}/wigwag/etc/edge-proxy.VERSION
-echo -n "kubelet: "; ${SNAP}/wigwag/system/bin/kubelet --version | cut -d' ' -f2
-echo -n "bouncer: "; cat ${SNAP}/wigwag/etc/bouncer.VERSION
+echo -n "relay-term: "; cat "${SNAP}/wigwag/etc/edge-node-modules.VERSION"
+echo -n "pe-utils: "; "${SNAP}/wigwag/pe-utils/identity-tools/developer_identity/create-dev-identity.sh" -V
+echo -n "edge-core: "; "${SNAP}/wigwag/mbed/edge-core" --version
+echo -n "devicedb: "; "${SNAP}/wigwag/system/bin/devicedb" -version
+echo -n "maestro: "; cat "${SNAP}/wigwag/etc/maestro.VERSION"
+echo -n "maestro-shell: "; cat "${SNAP}/wigwag/etc/maestro-shell.VERSION"
+echo -n "edge-proxy: "; cat "${SNAP}/wigwag/etc/edge-proxy.VERSION"
+echo -n "kubelet: "; "${SNAP}/wigwag/system/bin/kubelet" --version | cut -d' ' -f2
+echo -n "bouncer: "; cat "${SNAP}/wigwag/etc/bouncer.VERSION"
 if [ "${SHOW_DOCKER}" = "true" ]; then
-    echo -n "docker: "; ${SNAP}/bin/docker --version
-    echo -n "docker-runc: "; ${SNAP}/bin/docker-runc --version
-    echo -n "docker-containerd: "; ${SNAP}/bin/docker-containerd --version
-    echo -n "docker-init: "; ${SNAP}/bin/docker-init --version
+    echo -n "docker: "; "${SNAP}/bin/docker" --version
+    echo -n "docker-runc: "; "${SNAP}/bin/docker-runc" --version
+    echo -n "docker-containerd: "; "${SNAP}/bin/docker-containerd" --version
+    echo -n "docker-init: "; "${SNAP}/bin/docker-init" --version
 fi
