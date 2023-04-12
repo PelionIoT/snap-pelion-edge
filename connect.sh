2456f830672f4f58960fdaa09815dcb054831988
Janne Kiiskilä <janne.kiiskila@izumanetworks.com>
2023-04-12 17:26:28 +0300

    Fix apparmor complaints
    Add a lot of plugs to the snapcraf.yaml to clear out apparmor complaints.
    You can see them easily with snappy-debug and it even advices what you
    should do to fix them. This clears out almost all of them, you will
    still see a few between doing the snap install and running connect.sh,
    but you should not see any after the reboot.

--- connect.sh
+++ connect.sh
@@ -20,29 +20,40 @@
 
 SNAP_NAME="${SNAP_NAME:-pelion-edge}"
 
-sudo snap connect "${SNAP_NAME}:snapd-control"       :snapd-control
-sudo snap connect "${SNAP_NAME}:modem-manager"       modem-manager:service
-sudo snap connect "${SNAP_NAME}:network-manager"     network-manager:service
-sudo snap connect "${SNAP_NAME}:network-control"     :network-control
-sudo snap connect "${SNAP_NAME}:privileged"          :docker-support
-sudo snap connect "${SNAP_NAME}:support"             :docker-support
-sudo snap connect "${SNAP_NAME}:firewall-control"    :firewall-control
-sudo snap connect "${SNAP_NAME}:docker-cli"          "${SNAP_NAME}:docker-daemon"
-sudo snap connect "${SNAP_NAME}:log-observe"         :log-observe
-sudo snap connect "${SNAP_NAME}:system-files-logs"   :system-files
-sudo snap connect "${SNAP_NAME}:kernel-module-observe" :kernel-module-observe
-sudo snap connect "${SNAP_NAME}:system-trace"        :system-trace
-sudo snap connect "${SNAP_NAME}:system-observe"      :system-observe
-sudo snap connect "${SNAP_NAME}:account-control"     :account-control
-sudo snap connect "${SNAP_NAME}:bluetooth-control"   :bluetooth-control
-sudo snap connect "${SNAP_NAME}:hardware-observe"    :hardware-observe
-sudo snap connect "${SNAP_NAME}:kubernetes-support"  :kubernetes-support
-sudo snap connect "${SNAP_NAME}:mount-observe"       :mount-observe
-sudo snap connect "${SNAP_NAME}:netlink-audit"       :netlink-audit
-sudo snap connect "${SNAP_NAME}:netlink-connector"   :netlink-connector
-sudo snap connect "${SNAP_NAME}:network-observe"     :network-observe
-sudo snap connect "${SNAP_NAME}:process-control"     :process-control
-sudo snap connect "${SNAP_NAME}:shutdown"            :shutdown
-sudo snap connect "${SNAP_NAME}:dbus-wpa"            wpa-supplicant:service
-sudo snap connect "${SNAP_NAME}:home"                :home
-sudo snap connect "${SNAP_NAME}:edge-tool-files"     :personal-files
+sudo snap connect "${SNAP_NAME}:snapd-control"              :snapd-control
+sudo snap connect "${SNAP_NAME}:modem-manager"              modem-manager:service
+sudo snap connect "${SNAP_NAME}:network-manager"            network-manager:service
+sudo snap connect "${SNAP_NAME}:network-control"            :network-control
+sudo snap connect "${SNAP_NAME}:privileged"                 :docker-support
+sudo snap connect "${SNAP_NAME}:support"                    :docker-support
+sudo snap connect "${SNAP_NAME}:firewall-control"           :firewall-control
+sudo snap connect "${SNAP_NAME}:docker-cli"                 "${SNAP_NAME}:docker-daemon"
+sudo snap connect "${SNAP_NAME}:log-observe"                :log-observe
+sudo snap connect "${SNAP_NAME}:system-files-logs"          :system-files
+sudo snap connect "${SNAP_NAME}:kernel-module-observe"      :kernel-module-observe
+sudo snap connect "${SNAP_NAME}:system-trace"               :system-trace
+sudo snap connect "${SNAP_NAME}:system-observe"             :system-observe
+sudo snap connect "${SNAP_NAME}:account-control"            :account-control
+sudo snap connect "${SNAP_NAME}:bluetooth-control"          :bluetooth-control
+sudo snap connect "${SNAP_NAME}:hardware-observe"           :hardware-observe
+sudo snap connect "${SNAP_NAME}:kubernetes-support"         :kubernetes-support
+sudo snap connect "${SNAP_NAME}:mount-observe"              :mount-observe
+sudo snap connect "${SNAP_NAME}:netlink-audit"              :netlink-audit
+sudo snap connect "${SNAP_NAME}:netlink-connector"          :netlink-connector
+sudo snap connect "${SNAP_NAME}:network-observe"            :network-observe
+sudo snap connect "${SNAP_NAME}:process-control"            :process-control
+sudo snap connect "${SNAP_NAME}:shutdown"                   :shutdown
+sudo snap connect "${SNAP_NAME}:home"                       :home
+sudo snap connect "${SNAP_NAME}:edge-tool-files"            :personal-files
+sudo snap connect "${SNAP_NAME}:usr-libexec-kubernetes"     :system-files
+sudo snap connect "${SNAP_NAME}:run-docker"                 :system-files
+sudo snap connect "${SNAP_NAME}:fuse-support"               :fuse-support
+sudo snap connect "${SNAP_NAME}:steam-support"              :steam-support
+sudo snap connect "${SNAP_NAME}:etc-apparmor-abi"           :system-files
+sudo snap connect "${SNAP_NAME}:removable-media"            :removable-media
+
+# Only try connecting wpa if it is available (i.e. device has Wi-Fi)
+wpa=$(snap connections |grep wpa-supplicant) || true
+if [[ -n "${wpa}" ]]; then
+    sudo snap connect "${SNAP_NAME}:dbus-wpa"               wpa-supplicant:service
+fi
