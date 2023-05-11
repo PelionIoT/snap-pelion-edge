49b254ab5d2cb1dac4d83958f05bf7d4e343aefb
Janne Kiiskilä <janne.kiiskila@izumanetworks.com>
2023-05-11 11:51:46 +0300

    print-versions.sh - remove devicedb
    It's not part of the image anymore, so no point looking for it.

--- files/version/print-versions.sh
+++ files/version/print-versions.sh
@@ -13,7 +13,6 @@ done
 echo -n "pe-terminal: "; cat "${SNAP}/wigwag/etc/pe-terminal.VERSION"
 echo -n "pe-utils: "; "${SNAP}/wigwag/pe-utils/identity-tools/developer_identity/create-dev-identity.sh" -V
 echo -n "edge-core: "; "${SNAP}/wigwag/mbed/edge-core" --version
-echo -n "devicedb: "; "${SNAP}/wigwag/system/bin/devicedb" -version
 echo -n "maestro: "; cat "${SNAP}/wigwag/etc/maestro.VERSION"
 echo -n "maestro-shell: "; cat "${SNAP}/wigwag/etc/maestro-shell.VERSION"
 echo -n "edge-proxy: "; cat "${SNAP}/wigwag/etc/edge-proxy.VERSION"
